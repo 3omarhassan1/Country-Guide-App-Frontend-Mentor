962ff52938976a3531a647588e5852a14cdd49c1
Omar Hassan <omar.eg55@yahoo.com>
2023-01-14 20:46:24 +0200

    Deployed website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
